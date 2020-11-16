a0dbf38147518c89b1517cab90145ae46994f172
FridahAlwanga <fridahalwanga6@gmail.com>
2020-11-16 16:04:44 +0300

    add about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SearchFormComponent } from './search-form/search-form.component';
-import { from } from 'rxjs';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/forms'
+// import { HttpClientModule } from '@angular/common/http';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { SearchFormComponent } from './search-form/search-form.component';
+// import { from } from 'rxjs';
+// import { AboutComponent } from './about/about.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    ProfileComponent,
-    SearchFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-export class AppBootstrapModule { }
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     NavbarComponent,
+//     FooterComponent,
+//     ProfileComponent,
+//     SearchFormComponent,
+//     AboutComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     HttpClientModule,
+//     FormsModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+// export class AppBootstrapModule { }
--- src/app/repositories.ts
+++ src/app/repositories.ts
@@ -1,14 +1,14 @@
-export class Repositories {
-    constructor(
-        public login: string,
-        public avatar_url: string,
-        public followers: number,
-        public following: number,
-        public bio: string,
-        public location: string,
-        public email: string,
-        public public_repos: string,
-    ) {
+// export class Repositories {
+//     constructor(
+//         public login: string,
+//         public avatar_url: string,
+//         public followers: number,
+//         public following: number,
+//         public bio: string,
+//         public location: string,
+//         public email: string,
+//         public public_repos: string,
+//     ) {
 
-    }
-}
+//     }
+// }
