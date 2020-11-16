01b551ce96c0056573fece9c4e192a114d5e8b3b
FridahAlwanga <fridahalwanga6@gmail.com>
2020-11-16 16:21:49 +0300

    imported and exported app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms'
-// import { HttpClientModule } from '@angular/common/http';
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { NavbarComponent } from './navbar/navbar.component';
-// import { FooterComponent } from './footer/footer.component';
-// import { ProfileComponent } from './profile/profile.component';
-// import { SearchFormComponent } from './search-form/search-form.component';
-// import { from } from 'rxjs';
-// import { AboutComponent } from './about/about.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { from } from 'rxjs';
+import { AboutComponent } from './about/about.component';
 
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     NavbarComponent,
-//     FooterComponent,
-//     ProfileComponent,
-//     SearchFormComponent,
-//     AboutComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     HttpClientModule,
-//     FormsModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-// export class AppBootstrapModule { }
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileComponent,
+    SearchFormComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+export class AppBootstrapModule { }
