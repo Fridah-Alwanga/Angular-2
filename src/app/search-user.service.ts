3b1928293e4ef3bdcb3433f0e86c09e7a9e4f783
FridahAlwanga <fridahalwanga6@gmail.com>
2020-11-16 16:23:35 +0300

    updated class search-user

--- src/app/search-user.service.ts
+++ src/app/search-user.service.ts
@@ -1,78 +1,78 @@
-// import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-// import { HttpClient } from '@angular/common/http';
-// import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
-// import { User } from './user'
-// import { Repositories } from './repositories'
+import { User } from './user'
+import { Repositories } from './repositories'
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class SearchUserService {
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchUserService {
 
-//   userProfile: User
+  userProfile: User
 
-//   userRepo: Repositories
+  userRepo: Repositories
 
   
   
-//   constructor(private http: HttpClient) {
-//     this.userProfile = new User('', '', 0, 0, '', '', '', '');
+  constructor(private http: HttpClient) {
+    this.userProfile = new User('', '', 0, 0, '', '', '', '');
 
-//   }
+  }
 
-//   searchUser(user) {
-//     interface Responsee {
-//       login: string,
-//       avatar_url: string,
-//       followers: number,
-//       following: number,
-//       bio: string,
-//       location: string,
-//       email: string,
-//       public_repos: string,
+  searchUser(user) {
+    interface Responsee {
+      login: string,
+      avatar_url: string,
+      followers: number,
+      following: number,
+      bio: string,
+      location: string,
+      email: string,
+      public_repos: string,
 
-//     }
+    }
 
-//     let baseUrl = environment.API_URL + user + '?access_token=' + environment.API_KEY;
-//     let promise = new Promise((resolve, reject) => {
-//       this.http.get<Responsee>(baseUrl).toPromise().then(res => {
-//         this.userProfile = res;
-//         //console.log(res);
-//         resolve()
+    let baseUrl = environment.API_URL + user + '?access_token=' + environment.API_KEY;
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<Responsee>(baseUrl).toPromise().then(res => {
+        this.userProfile = res;
+        //console.log(res);
+        resolve()
 
-//       }, error => {
-//         reject();
-//       })
+      }, error => {
+        reject();
+      })
 
-//     });
-//     return promise
-//   }
+    });
+    return promise
+  }
 
-//   displayRepos(user) {
-//     interface apiResponse {
-//       name: string,
-//       description: string,
-//       html_url: string,
-//       language: string
+  displayRepos(user) {
+    interface apiResponse {
+      name: string,
+      description: string,
+      html_url: string,
+      language: string
 
-//     }
+    }
 
-//     let url = environment.API_URL + user + '/repos' + '?access_token=' + environment.API_KEY;
-//     let promise = new Promise((resolve, reject) => {
-//       this.http.get<apiResponse>(url).toPromise().then(response => {
-//         // this.userRepo = response
-//         console.log(response)
-//         resolve()
+    let url = environment.API_URL + user + '/repos' + '?access_token=' + environment.API_KEY;
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<apiResponse>(url).toPromise().then(response => {
+        // this.userRepo = response
+        console.log(response)
+        resolve()
 
-//       }, error => {
-//         reject();
-//       })
+      }, error => {
+        reject();
+      })
 
-//     });
-//     return promise
-//   }
+    });
+    return promise
+  }
 
 
-// }
+}
