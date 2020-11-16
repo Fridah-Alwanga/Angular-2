818bb787d75901f84f40b0c214ad65284ac5c244
FridahAlwanga <fridahalwanga6@gmail.com>
2020-11-16 16:17:06 +0300

    updated ptofile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,48 +1,48 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
-// import { SearchUserService } from '../search-user.service'
-// import { Repositories } from '../repositories';
-// import { User } from '../user';
+import { SearchUserService } from '../search-user.service'
+import { Repositories } from '../repositories';
+import { User } from '../user';
 
-// @Component({ 
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
-// export class ProfileComponent implements OnInit {
+@Component({ 
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
 
-//   userProfile: User
-//   userRepo: Repositories
+  userProfile: User
+  userRepo: Repositories
 
-//   constructor(private SearchUserService: SearchUserService) {
+  constructor(private SearchUserService: SearchUserService) {
  
-//   }
+  }
 
-//   searchUser(user) {
+  searchUser(user) {
 
-//     this.SearchUserService.searchUser(user).then((succes) => {
-//       this.userProfile = this.SearchUserService.userProfile;
-//     },
-//       (error) => {
-//         console.log(error)
-//       }
-//     )
+    this.SearchUserService.searchUser(user).then((succes) => {
+      this.userProfile = this.SearchUserService.userProfile;
+    },
+      (error) => {
+        console.log(error)
+      }
+    )
 
-//     this.SearchUserService.displayRepos(user).then((succes) => {
-//       this.userRepo = this.SearchUserService.userRepo;
-//     },
-//       (error) => {
-//         console.log(error)
-//       }
-//     )
+    this.SearchUserService.displayRepos(user).then((succes) => {
+      this.userRepo = this.SearchUserService.userRepo;
+    },
+      (error) => {
+        console.log(error)
+      }
+    )
 
-//   }
+  }
 
-//   ngOnInit(): void {
+  ngOnInit(): void {
 
-//     this.searchUser('Fridah-Alwanga');
-//   }
-// }
+    this.searchUser('Fridah-Alwanga');
+  }
+}
 
 
